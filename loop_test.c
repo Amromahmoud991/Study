7cdfa863fc6b74c55078c7c3be40ccce3799a200
amro <amromahmoud991@gmail.com>
2026-09-15 20:57:07 +0300

    small change to prevent glitches

--- loop_test.c
+++ loop_test.c
@@ -6,7 +6,7 @@ int main(void)
     int number;
     int i;
     printf ("What symbol (# or *) do you want?  ");
-    scanf ("%c", &symbol);
+    scanf (" %c", &symbol);
     printf ("how many of %c do you want? ", symbol);
     scanf ("%d", &number);
     for (i=0; i<number; i++) {
